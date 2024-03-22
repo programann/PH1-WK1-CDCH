a003e89a4d06374c5ee277a9875be02eb5a1cd2a
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-03-23 02:04:07 +0300

    phase1 week1 challenge

--- studentgrade.js
+++ studentgrade.js
@@ -1,16 +1,23 @@
-function userGrade(userInput){
+//let holds the perfomance
+//For marks :>79 return performance to be "A";
+            // :>=60 && <=79  performance to be "B";
+            // :>=49 && <= 59 performance to be "C";
+            // ;>=40 && < 49 Performance to be "D";
+            // :<40 && >=0 performance to be "E"
 
-    let finalGrade = "E"
-
-    if(userInput >= 0 && userInput < 40){
-        finalGrade = "E"
-    }else if(userInput >= 40 && userInput < 49){
-        finalGrade = "D"
-    }else if(userInput >= 49 && userInput <= 59){
-        finalGrade = "C"
-    }else if(userInput >= 60 && userInput <= 79){
-        finalGrade = "B"
-    }else if(userInput > 79 && userInput <= 100){
-        finalGrade = "A"
+function Grade(Marks){
+    if(Marks>= 0 && Marks < 40){ //range =0 to 39
+        console.log(performance = "E");
+    }else if(Marks >= 40 && Marks < 49){ // range =40 to 48
+        console.log(Performance = "D");
+    }else if(Marks >= 49 && Marks <= 59){ // range = 49 to 59
+        console.log(Performance = "C");
+    }else if(Marks >= 60 && Marks <= 79){ // range = 60 to 79
+        console.log(performance = "B");
+    }else if(Marks > 79 && Marks<= 100){ // range 80 to 100
+        console.log(performance = "A");
+    }else{
+        console.log("Invalid mark")
     }
+
 }
